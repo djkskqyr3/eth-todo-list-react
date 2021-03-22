0c0e37bf59fdb6cdac6272e79681b879d9c3561d
JamesChan <djkskqyr3@hotmail.com>
2021-03-22 20:44:14 +0800

    integrated with smart contract

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
 import './App.css'
+import TodoListView from './TodoListView'
+import TodoList from './abis/TodoList.json'
+import Navbar from './Navbar'
 
 class App extends Component {
-  componentWillMount() {
-    this.loadBlockchainData()
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
   }
 
   async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
+    const web3 = window.web3
     const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    this.setState({account: accounts[0]})
+
+    const networkId = await web3.eth.net.getId()    
+    const networkData = TodoList.networks[networkId]
+    if (networkData) {
+      const todoList = new web3.eth.Contract(TodoList.abi, networkData.address)
+      this.setState({ todoList })
+      const taskCount = await todoList.methods.taskCount().call()
+      this.setState({ taskCount })
+      for (var i = 1; i <= taskCount; i++) {
+        const task = await todoList.methods.tasks(i).call()
+        this.setState({
+          tasks: [...this.state.tasks, task]
+        })
+      }
+      this.setState({ loading: false })
+    } else {
+      window.alert('Marketplace contract not deployed to detect network')
+    }    
+  }
+
+  createTask(content) {
+    this.setState({ loading: true })
+    this.state.todoList.methods.createTask(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+    window.location.reload()
   }
 
   constructor(props) {
     super(props)
-    this.state = { account: '' }
+    this.state = { 
+      account: '', 
+      taskCount: 0,
+      tasks: [],
+      loading: true
+    }
+    this.createTask = this.createTask.bind(this)
   }
 
   render() {
     return (
-      <div className="container">
-        <h1>Hello, World!</h1>
-        <p>Your account: {this.state.account}</p>
+      <div>
+        <Navbar account={this.state.account} />
+        <div className="container-fluid">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex justify-content-center">
+            { this.state.loading
+              ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+              : <TodoListView tasks={this.state.tasks} createTask={this.createTask} />
+            }
+            </main>
+          </div>
+        </div>
       </div>
     );
   }
